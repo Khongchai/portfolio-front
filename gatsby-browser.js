edbdd743b2eaa319c8df415d14eba8357c193782
khongchai <world1955@hotmail.com>
2021-07-13 18:10:02 +0700

    added backend link for production

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,10 @@ import { createClient, Provider } from "urql";
 import React from "react";
 
 const client = createClient({
-  url: "http://localhost:4000/graphql",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "https://khong-portfolio-backend.herokuapp.com/",
 });
 
 export const wrapRootElement = ({ element }) => {
